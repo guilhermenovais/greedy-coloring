5ec40450c82abf57c3487cf9778c0768fa6c4f57
Guilherme Novais de Souza <guilhermenovaisdesouza@gmail.com>
2023-11-05 10:50:27 -0300

    feat: criando cabeçalho da classe vértice

---
+++ include/vertice.hpp
@@ -0,0 +1,16 @@
+#ifndef VERTICE_HPP
+#define VERTICE_HPP
+
+// Forward declaration de ListaEncadeada
+class ListaEncadeada;
+
+class Vertice {
+    public:
+        Vertice();
+    private:
+        ListaEncadeada* adjacentes;
+        int rotulo;
+        int cor;
+};
+
+#endif // VERTICE_HPP
