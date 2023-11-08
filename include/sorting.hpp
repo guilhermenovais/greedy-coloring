1f48b4c3c2041ca465f5e6acbfaee566c0d93681
Guilherme Novais <guilhermenovaisdeosouza@proton.me>
2023-11-08 18:29:18 -0300

    feat: criando especificação da classe Sorting

---
+++ include/sorting.hpp
@@ -0,0 +1,17 @@
+#ifndef SORTING_HPP
+#define SORTING_HPP
+
+#include "vertice.hpp"
+
+class Sorting {
+    public:
+        static void bubbleSort(ListaEncadeada* lista);
+        static void selectionSort(ListaEncadeada* lista);
+        static void insertionSort(ListaEncadeada* lista);
+        static void quicksort(ListaEncadeada* lista);
+        static void mergesort(ListaEncadeada* lista);
+        static void heapsort(ListaEncadeada* lista);
+        static void customSort(ListaEncadeada* lista);
+};
+
+#endif // SORTING_HPP
