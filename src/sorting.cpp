af2b7d67766a17bc9ee926147eaeb480ac5e79cb
Guilherme Novais <guilhermenovaisdeosouza@proton.me>
2023-11-08 18:42:22 -0300

    feat: criando esqueleto para as funções de ordenação

---
+++ src/sorting.cpp
@@ -0,0 +1,29 @@
+#include "../include/sorting.hpp"
+
+void Sorting::bubbleSort(ListaEncadeada *lista) {
+
+}
+
+void Sorting::selectionSort(ListaEncadeada *lista) {
+
+}
+
+void Sorting::insertionSort(ListaEncadeada *lista) {
+
+}
+
+void Sorting::quicksort(ListaEncadeada *lista) {
+
+}
+
+void Sorting::mergesort(ListaEncadeada *lista) {
+
+}
+
+void Sorting::heapsort(ListaEncadeada *lista) {
+
+}
+
+void Sorting::customSort(ListaEncadeada *lista) {
+    
+}
