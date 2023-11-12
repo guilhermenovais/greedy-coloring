f202a344603a30df0be6c2a87ba14e231e06cf7d
Guilherme Novais de Souza <guilhermenovaisdesouza@gmail.com>
2023-11-12 09:09:21 +0000

    chore: adicionando comentários na main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "../include/grafo.hpp"
 
 int main(void) {
+    // Obtém os parâmetros para a execução do programa
     std::string linha;
     std::getline(std::cin, linha);
     std::istringstream stream(linha);
@@ -11,8 +12,10 @@ int main(void) {
     stream >> tipoOrdenacao;
     stream >> qtdVertices;
 
+    // Inicializa o grafo com os vértices
     Grafo grafo = Grafo(std::stoi(qtdVertices));
 
+    // Define os vértices adjacentes de cada vértice
     int rotulo = 0;
     while(rotulo < std::stoi(qtdVertices)) {
         std::getline(std::cin, linha);
@@ -20,12 +23,17 @@ int main(void) {
         rotulo++;
     }
 
+    // Define as cores de cada vértice
     std::getline(std::cin, linha);
     grafo.colore(linha);
 
+    // Ordena a lista de vértices usando o método de ordenação selecionado
     grafo.ordena(tipoOrdenacao[0]);
 
+    // Verifica se a coloração pode ser obtida usando um algoritmo guloso
     bool eGuloso = grafo.eGuloso();
+
+    // Imprime o resultado
     std::cout << eGuloso;
     if(eGuloso) {
         grafo.imprimeVertices();
